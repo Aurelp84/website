8a704d3f7a474e963134b0bf8c77684073d6131f
Aurelp84 <72976834+Aurelp84@users.noreply.github.com>
2025-02-27 21:30:54 +0100

    comm script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.dropdown').forEach(drop => {
-        drop.addEventListener("mouseover", function() {
-            this.querySelector(".dropdown-content").style.display = "block";
-        });
-        drop.addEventListener("mouseleave", function() {
-            this.querySelector(".dropdown-content").style.display = "none";
-        });
-    });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.querySelectorAll('.dropdown').forEach(drop => {
+//         drop.addEventListener("mouseover", function() {
+//             this.querySelector(".dropdown-content").style.display = "block";
+//         });
+//         drop.addEventListener("mouseleave", function() {
+//             this.querySelector(".dropdown-content").style.display = "none";
+//         });
+//     });
+// });
